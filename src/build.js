c354f5ace2af638c1b269fed2f349d1ea7745e69
Brian M. Workman <1266935+gesslar@users.noreply.github.com>
2025-11-05 21:56:11 -0500

    some

--- src/build.js
+++ src/build.js
@@ -1,54 +1,78 @@
-const vscode = require('vscode');
-const config = require('./config');
-
-const msg = vscode.window.showInformationMessage;
+const vscode = require("vscode")
+const config = require("./config.js")
+const assert = require("assert")
+const msg = vscode.window.showInformationMessage
 
 const builder = {
-  getBuildCommand: function (type) {
+  /**
+   * Get the build command
+   * @param {string} type - The type of build to run
+   * @returns {string|null} - The build command
+   */
+  _getBuildCommand: function (type) {
     const muddler_path = config.getMuddlerPath()
     const os = config.getOs()
     let build_command = `${muddler_path}/muddle`
 
-    if (os === 'win32')
+    if (os === "win32")
       build_command = `${muddler_path}\\muddle.bat`
-    else if (os === 'linux' || os === 'darwin')
+    else if (os === "linux" || os === "darwin")
       build_command = `${muddler_path}/muddle`
     else {
-      vscode.window.showErrorMessage('Unsupported operating system.')
-      return
+      vscode.window.showErrorMessage("Unsupported operating system.")
+      return null
     }
 
-    if (type === 'watch')
-      build_command += ' -w'
+    if (type === "watch")
+      build_command += " -w"
     else if (type === "generate")
-      build_command += ' --generate'
+      build_command += " --generate"
 
     console.info(build_command)
     return build_command
   },
-  build: function (type) {
-    if(type === "watch")
-      msg('Building with Muddler in watch mode...')
-    else if (type === "generate")
-      msg('Generating Muddler project...')
-    else
-      msg('Building with Muddler...')
 
-    const buildCommand = this.getBuildCommand(type)
+  /**
+   * Build the project
+   * @param {string|undefined} [type] - The type of build to run. Accepts "watch",
+   *                                  "generate", or undefined.
+   */
+  build: function (type = undefined) {
+    assert(type === "watch" || type === "generate" || type === undefined, "Invalid build type")
+
+    if(type === "watch")
+      msg("Building with Muddler in watch mode...")
+    else if (type === "generate")
+      msg("Generating Muddler project...")
+    else
+      msg("Building with Muddler...")
+
+    const buildCommand = this._getBuildCommand(type)
 
     if (!buildCommand) {
-      vscode.window.showErrorMessage('Muddler binary not found. Please download or set the path in settings.')
+      vscode.window.showErrorMessage("Muddler not found. Please download or set the path in settings.")
       return
     }
 
     // Reuse terminal if it exists
-    let terminal = vscode.window.terminals.find(t => t.name === 'Muddler Build')
+    let terminal = vscode.window.terminals.find(t => t.name === "Muddler Build")
     if (!terminal)
-      terminal = vscode.window.createTerminal('Muddler Build');
+      terminal = vscode.window.createTerminal("Muddler Build")
 
     terminal.show()
     terminal.sendText(buildCommand)
   },
+
+  /**
+   * Build the project in watch mode
+   */
+  watch: function () {
+    this.build("watch")
+  },
+
+  /**
+   * Generate the project
+   */
   generate: function () {
     this.build("generate")
   }
--- src/config.js
+++ src/config.js
@@ -1,18 +1,30 @@
-const vscode = require('vscode')
-const os = require('os')
+const vscode = require("vscode")
+const os = require("os")
 
 const config = {
+  /**
+   * Get the Muddler path
+   * @returns {string|null} - The Muddler path
+   */
   getMuddlerPath: function() {
-    const config = vscode.workspace.getConfiguration('muddlit')
-    const muddlerPath = config.get('muddlerPath')
+    const config = vscode.workspace.getConfiguration("muddlit")
+    const muddlerPath = config.get("muddlerPath")
     return muddlerPath || null
   },
 
+  /**
+   * Update the Muddler path
+   * @param {string} path - The new Muddler path
+   */
   updateMuddlerPath: function(path) {
-    const config = vscode.workspace.getConfiguration('muddlit')
-    config.update('muddlerPath', path, vscode.ConfigurationTarget.Global)
+    const config = vscode.workspace.getConfiguration("muddlit")
+    config.update("muddlerPath", path, vscode.ConfigurationTarget.Global)
   },
 
+  /**
+   * Get the operating system
+   * @returns {string} - The operating system
+   */
   getOs: function() {
     return os.platform()
   }
